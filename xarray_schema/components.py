4a586e016e388a8124ae9adc0452b9f605c8af09
Callan Gray <cal.j.gray@gmail.com>
2025-10-13 18:08:22 +0800

    Add CoordsSchema test cases

--- xarray_schema/components.py
+++ xarray_schema/components.py
@@ -349,7 +349,7 @@ class AttrSchema(BaseSchema):
 
         if self.value is not None:
             if self.value is not None and self.value != attr:
-                raise SchemaError(f'value {attr} != {self.value}')
+                raise SchemaError(f'attrs {attr} != {self.value}')
 
     @property
     def json(self) -> dict:
